ace9bbede76af5ba0c8294c526c2f756acd27d46
Kyle Hoefer <khoefer100@gmail.com>
2020-04-11 13:54:28 -0700

    Updated GoalList
    Added image to Goals List

--- PC-Watch/PC-Watch WatchKit Extension/Views/GoalList.swift
+++ PC-Watch/PC-Watch WatchKit Extension/Views/GoalList.swift
@@ -12,28 +12,37 @@ struct GoalList: View {
 
     @State var data = [Value]()
     
-    var body: some View {
-        NavigationLink(destination:
-            VStack(alignment: .leading) {
-                    Text("Goals")
-                        .bold()
-                    List (data, id: \.mapValue.fields.id.stringValue) { item in
-                        if !item.mapValue.fields.isPersistent.booleanValue
-                        && item.mapValue.fields.isAvailable.booleanValue {
-                        Text(item.mapValue.fields.title.stringValue)
+        var body: some View {
+            NavigationLink(destination:
+                VStack(alignment: .leading) {
+                        Text("Goals")
+                            .bold()
+                        List (data, id: \.mapValue.fields.id.stringValue) { item in
+                            
+                            if !item.mapValue.fields.isPersistent.booleanValue
+                            && item.mapValue.fields.isAvailable.booleanValue {
+                                
+                                if item.mapValue.fields.photo.stringValue != "" {
+                                    AsyncImage(
+                                        url: URL(string: item.mapValue.fields.photo.stringValue)!,
+                                        placeholder: Text("...")
+                                        ).aspectRatio(contentMode: .fit)
+                                }
+                                
+                            Text(item.mapValue.fields.title.stringValue)
+                        }
                     }
                 }
+                .onAppear { FirebaseServices().getFirebaseData() { (data) in
+                    self.data = data
+                    
+                    }
+                }) {
+                Text("Goals")
+                    .frame(height: 50, alignment: .center)
             }
-            .onAppear { FirebaseServices().getFirebaseData() { (data) in
-                self.data = data
-                
-                }
-            }) {
-            Text("Goals")
-                .frame(height: 50, alignment: .center)
         }
     }
-}
 
 struct GoalList_Previews: PreviewProvider {
     static var previews: some View {
