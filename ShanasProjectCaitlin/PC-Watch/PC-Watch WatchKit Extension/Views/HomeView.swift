9aafd3fc599da730ec7b718515fabfdf404e164f
Emma Allegrucci <emmallegrucci@gmail.com>
2020-07-08 15:03:28 -0700

    UI changes, need to integrate actual data

--- ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/HomeView.swift
+++ ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/HomeView.swift
@@ -9,16 +9,28 @@
 import SwiftUI
 
 struct TaskCompleteImage: View {
-    //TODO: Add some logic to TaskComplete so open circle is not done
-    //and checkmark is done
+    var isComplete: Bool
+    
     var body : some View {
-        Image("check")
-        .resizable()
-        .frame(width: 20, height: 20)
-        .clipShape(Circle())
-        .overlay(Circle().stroke(Color.white, lineWidth: 0.5))
-        .shadow(radius: 10)
-            .padding(EdgeInsets(top: 10, leading: 1, bottom: 10, trailing: 1.5))
+        ZStack {
+            if (self.isComplete) {
+                Image("check")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 0.5))
+                .shadow(radius: 10)
+                    .padding(EdgeInsets(top: 10, leading: 1, bottom: 10, trailing: 1.5))
+            } else {
+                Image("")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 0.5))
+                .shadow(radius: 10)
+                    .padding(EdgeInsets(top: 10, leading: 1, bottom: 10, trailing: 1.5))
+            }
+        }
     }
 }
 
@@ -38,6 +50,7 @@ struct GoalView: View {
     var itemID: String?
     var name: String?
     var time: String?
+    @State var isComplete: Bool
     
     var body: some View {
         NavigationLink(destination: TasksView(itemID: itemID)){
@@ -46,7 +59,7 @@ struct GoalView: View {
                     HStack {
                         Text(name!).fontWeight(.bold).font(.system(size: 20))
                         Spacer()
-                        TaskCompleteImage()
+                        TaskCompleteImage(isComplete: isComplete)
                     }
                 }
                 HStack {
@@ -62,6 +75,7 @@ struct RoutineView: View {
     var itemID: String?
     var name: String?
     var time: String?
+    @State var isComplete: Bool
     
     var body: some View {
         NavigationLink(destination: TasksView(itemID: itemID)){
@@ -70,7 +84,7 @@ struct RoutineView: View {
                     HStack {
                         Text(name!).fontWeight(.bold).font(.system(size: 20))
                         Spacer()
-                        TaskCompleteImage()
+                        TaskCompleteImage(isComplete: isComplete)
                     }
                 }
                 HStack {
@@ -86,6 +100,7 @@ struct EventView: View {
     var itemID: String?
     var name: String?
     var time: String?
+    @State var isComplete: Bool
     
     var body: some View {
         NavigationLink(destination: TasksView(itemID: itemID)){
@@ -94,7 +109,7 @@ struct EventView: View {
                     HStack {
                         Text(name!).fontWeight(.bold).font(.system(size: 20))
                         Spacer()
-                        TaskCompleteImage()
+                        TaskCompleteImage(isComplete: isComplete)
                     }
                 }
                 HStack {
@@ -139,12 +154,12 @@ struct HomeView: View {
                         ForEach(self.model.data!.filter{!($0.mapValue.fields.isPersistent.booleanValue)}, id: \.mapValue.fields.id.stringValue) { item in
                             VStack {
                                 if item.mapValue.fields.isAvailable.booleanValue {
-                                    GoalView(itemID:item.mapValue.fields.id.stringValue, name: item.mapValue.fields.title.stringValue, time: self.formatter.string(from: self.timeLeft.date(from: item.mapValue.fields.startDayAndTime.stringValue)!)  + " - " + self.formatter.string(from: self.timeLeft.date(from: item.mapValue.fields.endDayAndTime.stringValue)!))
+                                    GoalView(itemID:item.mapValue.fields.id.stringValue, name: item.mapValue.fields.title.stringValue, time: self.formatter.string(from: self.timeLeft.date(from: item.mapValue.fields.startDayAndTime.stringValue)!)  + " - " + self.formatter.string(from: self.timeLeft.date(from: item.mapValue.fields.endDayAndTime.stringValue)!), isComplete: false)
                                 }
                             }
                         }
                     }.listStyle(CarouselListStyle()).navigationBarTitle("My Day")
-                }.edgesIgnoringSafeArea(.bottom).padding(0)
+                }.padding(0)
             }
         }
     }
--- ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/TasksView.swift
+++ ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/TasksView.swift
@@ -12,18 +12,28 @@ struct TaskItem: View {
     var taskID: String?
     var itemID: String?
     var taskName: String?
+    @State var started = false
     
     var body: some View {
-        HStack{
-            NavigationLink(destination: StepsView(taskID: taskID!, itemID: itemID!)){
-                Text(taskName!)
-            }
-            Divider()
-            Text("Start")
-                .foregroundColor(.green)
-                .onTapGesture {
-                    print("Starting...")
+        VStack {
+            HStack{
+                NavigationLink(destination: StepsView(taskID: taskID!, itemID: itemID!)){
+                    Text(taskName!)
                 }
+                Spacer()
+                Divider()
+                if (!self.started) {
+                    Text("Start")
+                        .foregroundColor(.green)
+                        .onTapGesture {
+                            self.started = true
+                            print("Starting...")
+                        }
+                } else {
+                    Text("Started")
+                    .foregroundColor(.yellow)
+                }
+            }
         }
     }
 }
@@ -36,31 +46,46 @@ struct TasksView: View {
     
     var body: some View {
         GeometryReader { geo in
-            if (self.model.goalsSubtasks[self.itemID!] == nil) {
-                VStack {
-                    Text("No actions and tasks found!")
+            VStack {
+                Text("Plant flowers")
+                HStack {
+                    Text("10:00 - 11:00")
                     Spacer()
+                    Text("30 min")
                 }
-            }
-            else{
-                VStack {
-                    Text(self.name!)
-                    HStack {
-                        Text(self.time!)
-                        Spacer()
-                        Text("Duration")
-                    }
-                    List {
-                        ForEach(self.model.goalsSubtasks[self.itemID!]!!, id: \.mapValue.fields.id.stringValue) { item in
-                            VStack(alignment: .leading) {
-                                if item.mapValue.fields.isAvailable.booleanValue {
-                                    TaskItem(taskID: item.mapValue.fields.id.stringValue, itemID: self.itemID!, taskName: item.mapValue.fields.title.stringValue)
-                                }
-                            }
-                        }
-                    }.navigationBarTitle("Tasks")
-                }.edgesIgnoringSafeArea(.bottom)
-            }
+                List {
+                    TaskItem(taskID: "task1ID", itemID: "Plant flowers", taskName: "Task 1")
+                    TaskItem(taskID: "task2ID", itemID: "Plant flowers", taskName: "Task 2")
+                    TaskItem(taskID: "task3ID", itemID: "Plant flowers", taskName: "Task 3")
+                    TaskItem(taskID: "task4ID", itemID: "Plant flowers", taskName: "Task 4")
+                    TaskItem(taskID: "task5ID", itemID: "Plant flowers", taskName: "Task 5")
+                }.navigationBarTitle("Tasks")
+            }.padding(0)
+//            if (self.model.goalsSubtasks[self.itemID!] == nil) {
+//                VStack {
+//                    Text("No actions and tasks found!")
+//                    Spacer()
+//                }
+//            }
+//            else{
+//                VStack {
+//                    Text(self.name!)
+//                    HStack {
+//                        Text(self.time!)
+//                        Spacer()
+//                        Text("Duration")
+//                    }
+//                    List {
+//                        ForEach(self.model.goalsSubtasks[self.itemID!]!!, id: \.mapValue.fields.id.stringValue) { item in
+//                            VStack(alignment: .leading) {
+//                                if item.mapValue.fields.isAvailable.booleanValue {
+//                                    TaskItem(taskID: item.mapValue.fields.id.stringValue, itemID: self.itemID!, taskName: item.mapValue.fields.title.stringValue)
+//                                }
+//                            }
+//                        }
+//                    }.navigationBarTitle("Tasks")
+//                }.edgesIgnoringSafeArea(.bottom)
+//            }
         }
     }
 }
