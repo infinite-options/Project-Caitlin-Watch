0fad84a8f814a6561a2e26921ae9dbd9d8c107ca
Emma Allegrucci <emmallegrucci@gmail.com>
2020-07-20 14:02:46 -0700

    small updates

--- ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/HomeView.swift
+++ ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/HomeView.swift
@@ -135,29 +135,7 @@ struct HomeView: View {
                                         }
                                         Spacer()
                                         HStack {
-                                            //TODO: tell harshit there were too many conditionals
                                             itemImage(photo: item.mapValue.fields.photo.stringValue, isComplete: item.mapValue.fields.isComplete!.booleanValue, isInProgress: item.mapValue.fields.isInProgress!.booleanValue)
-//                                            if ((item.mapValue.fields.isComplete?.booleanValue) == true){
-//                                                SmallAssetImage(urlName: item.mapValue.fields.photo.stringValue, placeholder: Image("default-goal"))
-//                                                    .aspectRatio(contentMode: .fit)
-//                                                    .opacity(0.40)
-//                                                    .overlay(Image(systemName: "checkmark.circle")
-//                                                        .font(.system(size:44))
-//                                                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 0, trailing: 0))
-//                                                        .foregroundColor(.green))
-//                                            } else if (item.mapValue.fields.isInProgress?.booleanValue == true) {
-//                                                SmallAssetImage(urlName: item.mapValue.fields.photo.stringValue, placeholder: Image("default-goal"))
-//                                                    .aspectRatio(contentMode: .fit)
-//                                                    .opacity(0.40)
-//                                                    .overlay(Image(systemName: "arrow.2.circlepath.circle")
-//                                                        .font(.system(size:44))
-//                                                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 0, trailing: 0))
-//                                                        .foregroundColor(.yellow))
-//                                            } else {
-//                                                SmallAssetImage(urlName: item.mapValue.fields.photo.stringValue, placeholder: Image("default-goal"))
-//                                                    .aspectRatio(contentMode: .fit)
-//                                            }
-                                            //Text(formatter.string(from: timeLeft.date(from: self.item!.mapValue.fields.startDayAndTime.stringValue)!)  + " - " + formatter.string(from: timeLeft.date(from: self.item!.mapValue.fields.endDayAndTime.stringValue)!)).fontWeight(.light).font(.system(size: 15))
                                             VStack(alignment: .leading) {
                                                 Text("Takes " + item.mapValue.fields.expectedCompletionTime.stringValue)
                                                     .fontWeight(.light)
