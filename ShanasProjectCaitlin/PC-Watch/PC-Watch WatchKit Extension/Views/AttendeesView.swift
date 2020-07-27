2c79086c88e5c225e8dcddf76b861c895144f3df
Emma Allegrucci <emmallegrucci@gmail.com>
2020-07-27 14:10:03 -0700

    attendees view working

--- ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/AttendeesView.swift
+++ ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/AttendeesView.swift
@@ -9,32 +9,42 @@
 import SwiftUI
 
 
+struct AttendeeView: View {
+    var item: Attendent?
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "person.circle")
+                    .font(.system(size:44))
+                    .foregroundColor(.yellow)
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text(item!.email!)
+                        .font(.system(size: 18, design: .rounded))
+                    Text(item!.responseStatus!)
+                        .font(.system(size: 12, design: .rounded))
+                }
+            }
+            Divider()
+        }
+    }
+}
+
 struct AtendeesView: View {
     var event: Event?
     
     var body: some View {
         ScrollView([.vertical]) {
-//                if event!.attendees == nil {
-//                    VStack(alignment: .center) {
-//                        Text("No other atendees.")
-//                    }
-//                } else {
-                ForEach(self.event!.attendees!, id: \.email) { item in
-                    HStack {
-                        Image(systemName: "person.circle")
-                            .font(.system(size:44))
-                            .foregroundColor(.yellow)
-                        Spacer()
-//                        VStack(alignment: .leading) {
-                            Text("item.email")
-                                .font(.system(size: 18, design: .rounded))
-                            Text("item.responseStatus")
-                                .font(.system(size: 12, design: .rounded))
-//                        }
-                    }
-                    Divider()
+            if event!.attendees == nil {
+                VStack(alignment: .center) {
+                    Text("No other atendees.")
                 }
-//                }
+            } else {
+                ForEach(self.event!.attendees!, id: \.email) { item in
+                    AttendeeView(item: item)
+                }
+            }
         }.navigationBarTitle("Attendees")
     }
 }
