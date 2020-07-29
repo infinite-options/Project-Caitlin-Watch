a865ccd9744b8fadbefad28ce35fcd4ac87f42f1
Emma Allegrucci <emmallegrucci@gmail.com>
2020-07-29 13:29:59 -0700

    merge function checks for isDisplayedToday

--- ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Services/UserDay.swift
+++ ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Services/UserDay.swift
@@ -22,29 +22,34 @@ class UserDay: ObservableObject {
         
         var calendar = Calendar.current
         calendar.timeZone = .current
-        
-        while i<events?.count ?? -1 && j<goals?.count ?? -1{
-            let eventStart = calendar.dateComponents([.hour, .minute, .second], from: ISO8601DateFormatter().date(from: (events![i].start?.dateTime)!)!)
-            let goalStart = calendar.dateComponents([.hour, .minute, .second], from: DayDateObj.timeLeft.date(from: (goals![j].mapValue?.fields.startDayAndTime.stringValue)!)!)
-            
-            if calendar.date(from: eventStart)! < calendar.date(from: goalStart)! {
-                self.UserDayData.append(events![i])
-                i += 1
-            }
-            else{
-                self.UserDayData.append(goals![j])
-                j += 1
-            }
-        }
-        
-        while i<events?.count ?? -1 {
-            self.UserDayData.append(events![i])
-            i += 1
-        }
-        
-        while j<goals?.count ?? -1 {
-            self.UserDayData.append(goals![j])
-            j += 1
-        }
+        while i<events?.count ?? -1 && j<goals?.count ?? -1 {
+           let eventStart = calendar.dateComponents([.hour, .minute, .second], from: ISO8601DateFormatter().date(from: (events![i].start?.dateTime)!)!)
+           let goalStart = calendar.dateComponents([.hour, .minute, .second], from: DayDateObj.timeLeft.date(from: (goals![j].mapValue?.fields.startDayAndTime.stringValue)!)!)
+           
+           print(eventStart)
+           print(goalStart)
+           
+           if calendar.date(from: eventStart)! < calendar.date(from: goalStart)! {
+               self.UserDayData.append(events![i])
+               i += 1
+           } else {
+               if goals![j].mapValue!.fields.isDisplayedToday.booleanValue == true {
+                   self.UserDayData.append(goals![j])
+               }
+               j += 1
+           }
+       }
+       
+       while i<events?.count ?? -1 {
+           self.UserDayData.append(events![i])
+           i += 1
+       }
+       
+       while j<goals?.count ?? -1 {
+           if goals![j].mapValue!.fields.isDisplayedToday.booleanValue == true {
+               self.UserDayData.append(goals![j])
+           }
+           j += 1
+       }
     }
 }
