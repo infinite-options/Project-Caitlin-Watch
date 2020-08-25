5722c0af4d43d7127c8dbcca02f8be23b36fe045
Emma Allegrucci <emmallegrucci@gmail.com>
2020-08-24 20:04:18 -0700

    UI Edits for person view

--- ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/PersonView.swift
+++ ShanasProjectCaitlin/PC-Watch/PC-Watch WatchKit Extension/Views/PersonView.swift
@@ -12,39 +12,46 @@ struct PersonView: View {
     var person: ImportantPerson
     
     var body: some View {
-        VStack {
-            HStack(alignment: .center) {
-                VStack(alignment: .leading) {
-                    Text(self.person.fields.name.stringValue)
-                        .font(.system(size: 20, design: .rounded))
-                    Text(self.person.fields.relationship.stringValue)
-                        .fontWeight(.light)
-                        .font(.system(size: 15))
-                }
-                Spacer()
-                if self.person.fields.havePic.booleanValue == false {
-                    Image(systemName: "person.circle")
-                        .font(.system(size:50))
-                        .foregroundColor(.yellow)
-                }
-                else {
-                    AsyncImage(
-                        url: URL(string: self.person.fields.pic!.stringValue)!,
-                        placeholder: Image(systemName: "person.circle"))
-                }
+        VStack(alignment: .center) {
+            if self.person.fields.havePic.booleanValue == false {
+                Image(systemName: "person.circle")
+                    .font(.system(size:100))
+                    .foregroundColor(.yellow)
+            }
+            else {
+                AsyncImage(
+                    url: URL(string: self.person.fields.pic!.stringValue)!,
+                    placeholder: Image(systemName: "person.circle"))
             }
             Spacer()
             if self.person.fields.phoneNumber?.stringValue != "" {
-                Image(systemName: "phone.circle")
-                    .font(.system(size:50))
-                    .foregroundColor(.green)
-                    .onTapGesture {
-                        let number = "tel:" + self.person.fields.phoneNumber!.stringValue
-                        if let telURL = URL(string: number) {
-                            let wkExtension = WKExtension.shared()
-                            wkExtension.openSystemURL(telURL)
-                        }
+                HStack {
+                    VStack {
+                        Text(self.person.fields.name.stringValue)
+                            .font(.system(size: 20, design: .rounded))
+                        Text(self.person.fields.relationship.stringValue)
+                            .fontWeight(.light)
+                            .font(.system(size: 15))
                     }
+                    Spacer()
+                    Image(systemName: "phone.circle")
+                        .font(.system(size:50))
+                        .foregroundColor(.green)
+                        .onTapGesture {
+                            let number = "tel:" + self.person.fields.phoneNumber!.stringValue
+                            if let telURL = URL(string: number) {
+                                let wkExtension = WKExtension.shared()
+                                wkExtension.openSystemURL(telURL)
+                            }
+                        }
+                }
+            } else {
+                Text(self.person.fields.name.stringValue)
+                    .font(.system(size: 20, design: .rounded))
+                Text(self.person.fields.relationship.stringValue)
+                    .fontWeight(.light)
+                    .font(.system(size: 15))
+                
             }
         }.navigationBarTitle(self.person.fields.name.stringValue)
     }
